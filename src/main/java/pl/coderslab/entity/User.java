df6cf38319df5fe6a15aef5d3f3bf49b4e9d1790
wiktorkaminski <wiktor.kaminski@mixbox.pl>
2020-06-27 11:36:41 +0200

    read() added

--- src/main/java/pl/coderslab/entity/User.java
+++ src/main/java/pl/coderslab/entity/User.java
@@ -25,12 +25,29 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
     public void setPassword(String password) {
         this.password = password;
     }
-    // ---- end of getters and setters ----
+
+    // ---- constructors ----
+
+    public User () {
+    }
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+    public User(String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
 }
--- src/main/java/pl/coderslab/entity/UserDao.java
+++ src/main/java/pl/coderslab/entity/UserDao.java
@@ -1,8 +1,81 @@
 package pl.coderslab.entity;
 
+import pl.coderslab.utils.DbUtil;
+
+import java.sql.*;
+
 public class UserDao {
     private static final String CREATE_USER_QUERY = "INSERT INTO users (username, email, password) VALUES (?, ?, ? );";
-    private static final String READ_USER_QUERY = "SELECT * FROM users WHERE ? = ?;";
+    private static final String READ_USER_QUERY = "SELECT * FROM users WHERE columnName = ?;";
     private static final String UPDATE_USER_QUERY = "UPDATE users SET ? = ? WHERE ? = ?;";
     private static final String DELETE_USER_QUERY = "DELETE FROM users WHERE id = userId";
+
+    public static void main(String[] args) throws SQLException {
+//        User user1 = new User();
+//        user1.setEmail("tikiriki999@blabla.com");
+//        user1.setPassword("lalala");
+//        user1.setUserName("pl.costam");
+//        System.out.println(create(user1).getId());
+        User checkUser = read("tikiriki@blabla.com");
+        System.out.println(checkUser.getId());
+        System.out.println(checkUser.getUserName());
+        System.out.println(checkUser.getEmail());
+        System.out.println(checkUser.getPassword());
+//        System.out.println(read(12));
+    }
+
+    public static User create(User user) throws SQLException {
+        try (Connection dbConnect = DbUtil.getConnection()) {
+            PreparedStatement statement = null;
+            statement = dbConnect.prepareStatement(CREATE_USER_QUERY, statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getPassword());
+            statement.executeUpdate();
+            ResultSet rs = statement.getGeneratedKeys();
+            if (rs.next()) {
+                user.setId(rs.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static User read(int userId) throws SQLException {
+        try (Connection dbConnect = DbUtil.getConnection()) {
+            PreparedStatement statement = dbConnect.prepareStatement(READ_USER_QUERY.replace("columnName", "id"));
+            statement.setInt(1, userId);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                User toReturnUser = new User(resultSet.getString("username"), resultSet.getString("email"), resultSet.getString("password"));
+                toReturnUser.setId(resultSet.getInt("id"));
+                return toReturnUser;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+        public static User read(String email) throws SQLException {
+        try (Connection dbConnect = DbUtil.getConnection()) {
+            PreparedStatement statement = dbConnect.prepareStatement(READ_USER_QUERY.replace("columnName", "email"));
+            statement.setString(1, email);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                User toReturnUser = new User(resultSet.getString("username"), resultSet.getString("email"), resultSet.getString("password"));
+                toReturnUser.setId(resultSet.getInt("id"));
+                return toReturnUser;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
